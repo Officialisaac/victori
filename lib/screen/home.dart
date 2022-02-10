7c75d5bac3f1cb15c13c29e68b5133c506f4191e
BloomBytegh <bloombytegh@gmail.com>
2022-02-10 13:17:01 +0000

    background

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -75,58 +75,67 @@ class _MyHomePageState extends State<MyHomePage> {
       //     ),
       //   ),
       // ),
-      bottomNavigationBar: Container(
-        // padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-        constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(context).size.height / 14,
-            maxWidth: MediaQuery.of(context).size.width / 2,
-            minWidth: MediaQuery.of(context).size.width / 2),
-        alignment: Alignment.bottomCenter,
+      bottomNavigationBar: bottomBarItem(context),
+    );
+  }
+
+  Container bottomBarItem(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
         color: Colors.transparent,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.home),
-              iconSize: 40,
-              highlightColor: Colors.orange,
-              focusColor: Colors.orange,
-              disabledColor: Colors.grey,
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SearchPage()),
-                );
-              },
-              icon: const Icon(Icons.search),
-              iconSize: 40,
-              highlightColor: Colors.orange,
-              focusColor: Colors.orange,
-              disabledColor: Colors.grey,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.message),
-              iconSize: 40,
-              highlightColor: Colors.orange,
-              focusColor: Colors.orange,
-              disabledColor: Colors.grey,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.person),
-              iconSize: 40,
-              highlightColor: Colors.orange,
-              focusColor: Colors.orange,
-              disabledColor: Colors.grey,
-            ),
-          ],
+        border: Border.all(
+          width: 5.0,
+          color: Colors.transparent,
         ),
+      ),
+      constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.height / 14,
+          maxWidth: MediaQuery.of(context).size.width / 2,
+          minWidth: MediaQuery.of(context).size.width / 2),
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home),
+            iconSize: 40,
+            highlightColor: Colors.orange,
+            focusColor: Colors.orange,
+            disabledColor: Colors.grey,
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SearchPage()),
+              );
+            },
+            icon: const Icon(Icons.search),
+            iconSize: 40,
+            highlightColor: Colors.orange,
+            focusColor: Colors.orange,
+            disabledColor: Colors.grey,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.message),
+            iconSize: 40,
+            highlightColor: Colors.orange,
+            focusColor: Colors.orange,
+            disabledColor: Colors.grey,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person),
+            iconSize: 40,
+            highlightColor: Colors.orange,
+            focusColor: Colors.orange,
+            disabledColor: Colors.grey,
+          ),
+        ],
       ),
     );
   }
